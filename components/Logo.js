d3450d74938de2188d1c6e4fd4de93711885f3d0
Shafiul Azim <shafiulmizbah2@gmail.com>
2021-07-13 19:33:02 +0600

    Added contact page

--- components/Logo.js
+++ components/Logo.js
@@ -1,13 +1,13 @@
 import { useRouter } from 'next/router';
 import React from 'react'
-import styles from "../styles/navbar.module.scss";
+
 
 const Logo = () => {
     const {push} = useRouter();
     return (
-        <div  className={styles.logobox} onClick={()=> push("/")}>
-            <span className={styles.S}>S</span>
-            <span className={styles.A}>A</span>
+        <div  className="logobox" onClick={()=> push("/")}>
+            <span className="S">S</span>
+            <span className="A">A</span>
         </div>
     )
 }
--- pages/contact.js
+++ pages/contact.js
@@ -1,7 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Head from 'next/head'
+import styles from "../styles/contact.module.scss";
+import Logo from "../components/Logo"
 
 const contact = () => {
+    const [newMessage,setNewMessage] = useState({email:"",message : ""});
+    const [error,setError] = useState("");
+
+    const handleSubmit=(e)=>{
+        e.preventDefault();
+        if(newMessage.email && newMessage.message && newMessage.email.trim(" ") && newMessage.message.trim(" "))
+        {
+            setError("");
+            console.log(newMessage)
+            setNewMessage({email : "",message : ""})
+        }else{
+            setError("Please Enter valid input!");
+        }
+    }
+
+    const handleChange = (e)=>{
+        const name = e.target.name;
+        const value = e.target.value;
+
+        setNewMessage({...newMessage,[name] : value });
+    }
+    
     return (
         <>  
             <Head>
@@ -9,7 +33,27 @@ const contact = () => {
                 <meta name="description" content="A web designer & developer.Projection of your mind" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            contact
+            <section className={styles.contact}>
+                <div className={styles.layer}>
+                    <form className={styles.form} onSubmit={(e)=> handleSubmit(e)}>
+                        <Logo />
+                        
+                        {error ? <div className={styles.error}>{error}</div> : <div className={styles.error}>{" "}</div> }
+
+                        <input className={styles.input} type="email" placeholder="Email" value={newMessage.email} name="email" onChange={(e)=> handleChange(e)}/>
+
+                        <textarea className={styles.input} placeholder="Your message" value={newMessage.message} name="message"
+                        onChange={(e)=> handleChange(e)}
+                        />
+                        
+                        <button className="btn">Send message</button>
+                        <div className={styles.or}>Or</div>
+                        <div className={styles.social}>
+                            [ icons goes here! ]
+                        </div>
+                    </form>
+                </div>
+            </section>
         </>
     )
 }
