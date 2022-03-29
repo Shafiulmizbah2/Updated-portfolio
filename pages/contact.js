909c1ac8a27a102c0c76f0d0523f5fe05141f4a4
Shafiul Azim <shafiulmizbah2@gmail.com>
2022-03-29 22:15:30 +0600

    send email functionality added

--- pages/contact.js
+++ pages/contact.js
@@ -1,65 +1,113 @@
-import React, { useState } from 'react'
-import Head from 'next/head';
+import React, { useState } from "react";
+import Head from "next/head";
 import styles from "../styles/contact.module.scss";
-import Logo from "../components/Logo"
-import { useRouter } from 'next/router';
+import Logo from "../components/Logo";
+import { useRouter } from "next/router";
+import emailjs from "@emailjs/browser";
 
 const pofile = "https://www.facebook.com/profile.php?id=100012008747536";
 
 const Contact = () => {
-    const { push } = useRouter();
-    const [newMessage,setNewMessage] = useState({email:"",message : ""});
-    const [error,setError] = useState("");
+  const { push } = useRouter();
+  const [newMessage, setNewMessage] = useState({ email: "", message: "" });
+  const [error, setError] = useState("");
 
-    const handleSubmit=(e)=>{
-        e.preventDefault();
-        if(newMessage.email && newMessage.message && newMessage.email.trim(" ") && newMessage.message.trim(" "))
-        {
-            setError("");
-            console.log(newMessage)
-            setNewMessage({email : "",message : ""})
-        }else{
-            setError("Please Enter valid input!");
-        }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (
+      newMessage.email &&
+      newMessage.message &&
+      newMessage.email.trim(" ") &&
+      newMessage.message.trim(" ")
+    ) {
+      setError("");
+
+      emailjs
+        .sendForm(
+          "service_94etrxo",
+          "template_ri68hri",
+          e.target,
+          "QQbOPt4EPcSeJYqjW"
+        )
+        .then(
+          (result) => {
+            showStatus("Message send successfully.Thank you.");
+            // console.log(result.text);
+          },
+          (error) => {
+            showStatus(error.text);
+          }
+        );
+
+      setNewMessage({ email: "", message: "" });
+      e.target.reset();
+    } else {
+      setError("Please Enter valid input!");
     }
+  };
 
-    const handleChange = (e)=>{
-        const name = e.target.name;
-        const value = e.target.value;
+  const showStatus = async (msg) => {
+    setError(msg);
+    setTimeout(() => {
+      setError("");
+    }, 1000);
+  };
 
-        setNewMessage({...newMessage,[name] : value });
-    }
-    
-    return (
-        <>  
-            <Head>
-                <title>Contact me || Shafiul Azim</title>
-                <meta name="description" content="A web designer & developer.Projection of your mind" />
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-            <section className={styles.contact}>
-                <div className={styles.layer}>
-                    <form className={styles.form} onSubmit={(e)=> handleSubmit(e)}>
-                        <Logo />
-                        
-                        {error ? <div className={styles.error}>{error}</div> : <div className={styles.error}>{" "}</div> }
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
 
-                        <input className={styles.input} type="email" placeholder="Email" value={newMessage.email} name="email" onChange={(e)=> handleChange(e)}/>
+    setNewMessage({ ...newMessage, [name]: value });
+  };
 
-                        <textarea className={styles.input} placeholder="Your message" value={newMessage.message} name="message"
-                        onChange={(e)=> handleChange(e)}
-                        />
-                        
-                        <button className="btn">Send message</button>
-                        <div className={styles.or}>Or</div>
-                        <p className={styles.social} onClick={()=> push(pofile)}>
-                            Connect with <span>Facebook</span> 
-                        </p>
-                    </form>
-                </div>
-            </section>
-        </>
-    )
-}
+  return (
+    <>
+      <Head>
+        <title>Contact me || Shafiul Azim</title>
+        <meta
+          name="description"
+          content="A web designer & developer.Projection of your mind"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <section className={styles.contact}>
+        <div className={styles.layer}>
+          <form className={styles.form} onSubmit={(e) => handleSubmit(e)}>
+            <Logo />
+
+            {error ? (
+              <div className={styles.error}>{error}</div>
+            ) : (
+              <div className={styles.error}> </div>
+            )}
+
+            <input
+              className={styles.input}
+              type="email"
+              placeholder="Your Email"
+              value={newMessage.email}
+              name="email"
+              onChange={(e) => handleChange(e)}
+            />
+
+            <textarea
+              className={styles.input}
+              placeholder="Your message"
+              value={newMessage.message}
+              name="message"
+              onChange={(e) => handleChange(e)}
+            />
+
+            <button className="btn">Send message</button>
+            <div className={styles.or}>Or</div>
+            <p className={styles.social} onClick={() => push(pofile)}>
+              Connect with <span>Facebook</span>
+            </p>
+          </form>
+        </div>
+      </section>
+    </>
+  );
+};
 
 export default Contact;
