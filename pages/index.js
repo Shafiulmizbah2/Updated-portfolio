9d8b1a1fb1018845468c9659e6f6668e18701158
ShafiulAzim <shafiul.azim@selise.ch>
2023-08-31 19:32:07 +0600

    this is a test commit

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import Image from "next/image";
 import { useRouter } from "next/router";
 import styles from "../styles/about.module.scss";
 import Typical from "react-typical";
